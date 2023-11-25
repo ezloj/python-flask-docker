dff39148e8031e7a359745d69a8b43cca538692f
ZLOJ <jan.lutenko@gmail.com>
2023-11-25 14:57:40 +0100

    Add: image and Makefile

--- src/app.py
+++ src/app.py
@@ -13,5 +13,15 @@ def index():
         return render_template('error.html')
 
 
+@app.route("/dzban")
+def dzban():
+    try:
+        host_name = socket.gethostname()
+        host_ip = socket.gethostbyname(host_name)
+        return render_template('dzban.html', hostname=host_name, ip=host_ip)
+    except:
+        return render_template('error.html')
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
